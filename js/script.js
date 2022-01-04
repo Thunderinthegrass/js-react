807d0acb42d19300a6f5fee7470ba3d535c9b06b
ThunderInTheGrass <propeller8924@gmail.com>
2022-01-04 20:55:21 +0800

    app asking about movies

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
 'use strict';
 
 
-let z = 8;
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+  count: +numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+const lastFilms1 = prompt('Один из последних просмотренных фильмов?', '');
+const rating1 = prompt('На сколько вы его оцените?', '');
+const lastFilms2 = prompt('Один из последних просмотренных фильмов?', '');
+const rating2 = prompt('На сколько вы его оцените?', '');
+
+
+personalMovieDB.movies[lastFilms1] = rating1;
+personalMovieDB.movies[lastFilms2] = rating2;
+
+console.log(personalMovieDB);
 
